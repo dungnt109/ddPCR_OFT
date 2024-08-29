554d37eb3a889d817d2b67ece3b9c047c64fa756
dung nguyen <dungnt109@gmail.com>
2024-08-29 10:00:41 +0800

    1. Change default value of niqr, quantile.range 2. Add Dr Luyi 3. Ask to choose preset or calculated OFT/GUSB 4. Add QC calculation 5. Change GUS -> GUSB 6. Chagne FAIL -> WARNING 7. 3 digit precision -> 2 digit precision 8. Remove never use hl60.gus.concentration

--- script.R
+++ script.R
@@ -24,12 +24,8 @@ cat(paste(silence, "\n"))
 # 1. increase niqr
 # 2. decrease quantile.range
 # suggested default: 
-#niqr <- 3
-#quantile.range <- 0.02
-
-# the parameters below may not sensitive
-niqr <- 5
-quantile.range <- 0.005
+niqr <- 3
+quantile.range <- 0.02
 
 
 show_outliers <- TRUE
@@ -124,18 +120,19 @@ runType <- switch(
 	"2" = "relative", 
 	"relative")
 
-cat("\nVerified by?\n1. Prof Allen Yeoh\n2. Blank\n")
+cat("\nVerified by?\n1. Prof Allen Yeoh\n2. Dr Lu Yi\n3. Blank\n")
 answer <- readLines("stdin",n=1)
 
 
 verifier <- switch(
 	answer, 
 	"1" = "Prof Allen Yeoh",
-	"2" = "", 
+	"2" = "Dr Lu Yi", 
+	"3" = "",
 	answer)
 
 
-cat("\nRun by?\n1. Amanda Lee\n2. Huan Pei Tee\n3. Nurhilya\n4. Others\n5. Blank\n")
+cat("\nRun by?\n1. Amanda Lee\n2. Huan Pei Tee\n3. Nurhilya\n4. Dr Lu Yi\n5. Others\n6. Blank\n")
 answer <- readLines("stdin",n=1)
 
 if (answer == "4"){
@@ -147,7 +144,9 @@ if (answer == "4"){
 	"1" = "Amanda Lee", 
 	"2" = "Huan Pei Tee", 
 	"3" = "Nurhilya", 
-	"5" = "", 
+	"4" = "Dr Lu Yi",
+	"5" = "Others",
+	"6" = "", 
 	answer 
 	)
 }
@@ -162,7 +161,7 @@ run_date <- switch(
 	answer 
 	)
 
-cat("\nReported by?\n1. Shirley Kham\n2. Huan Pei Tee\n3. Nurhilya\n4. Others\n5. Blank\n")
+cat("\nReported by?\n1. Shirley Kham\n2. Huan Pei Tee\n3. Nurhilya\n4. Dr Lu Yi\n5. Others\n6. Blank\n")
 answer <- readLines("stdin",n=1)
 
 if (answer == "4"){
@@ -174,7 +173,9 @@ if (answer == "4"){
 		"1" = "Shirley Kham", 
 		"2" = "Huan Pei Tee", 
 		"3" = "Nurhilya", 
-		"5" = "", 
+		"4" = "Dr Lu Yi", 
+		"5" = "Others",
+		"6" = "", 
 		answer 
 		)
 }
@@ -211,10 +212,16 @@ if (runmode == "interactive"){
 
 cat(paste("Analyzing sample sheet", sample_sheet_file, "\n", sep=""))
 
-cat("Please key in the OFT/GUS baseline in HL60.\n")
+cat("Please key in the OFT/GUSB baseline ratio in HL60, or press Enter to use the calculated value from the test.\n")
 
 hl60_baseline <- trimws(readLines("stdin",n=1))
 hl60_baseline <- as.numeric(hl60_baseline)
+
+if (is.na(hl60_baseline)) {
+	manual_hl60_baseline <- FALSE 
+} else {
+	manual_hl60_baseline <- TRUE
+}
 
 
 cat("Please key in the HL60 dilution factor or hit enter to use the default value (100x).\n")
@@ -234,18 +241,8 @@ if (is.na(hl60.gus.dilutionX)) {
 
 
 
-
-
-#cat("Please key in the gus concentration (copies/uL, after dilution) of current HL60 batch:\n");
-#hl60.gus.concentration <- trimws(readLines("stdin",n=1))
-#hl60.gus.concentration <- as.numeric(hl60.gus.concentration)
-hl60.gus.concentration <- 0 
-
-
-
-
-
 #folder <- choose.dir()
+
 
 sample_sheet <- read.table(sample_sheet_file, header=TRUE, row.names=1)
 
@@ -256,7 +253,9 @@ row.col.combinations <- expand.grid(rownames(sample_sheet), colnames(sample_shee
 
 
 
+
 samples <- rep("", nrow(row.col.combinations))
+
 
 
 
@@ -272,10 +271,8 @@ names(samples) <- apply(row.col.combinations, 1, paste, collapse="")
 
 
 
-
 #### get the files
 files <- list.files(folder, pattern = "*.csv")
-
 
 
 
@@ -290,6 +287,7 @@ names(files) <- well.position
 #### get the control
 ## get GUS, HL60
 hl60.gus <- samples[grepl("^HL60_GUS_", samples)]    ##MNC_ALB  => HL60_GUS 
+
 
 hl60.gus.file <- paste(folder, files[names(hl60.gus)], sep=separator) 
 
@@ -315,6 +313,19 @@ h2o.gus.int <- read.csv(h2o.gus.file, header=TRUE)
 h2o.gus.int <- h2o.gus.int[sample(nrow(h2o.gus.int)), ]
 
 
+### get QC list
+qc.marker.samples <- samples[grepl("^QC_OFT_", samples)]                                #
+qc.marker.files <- paste(folder, files[names(qc.marker.samples)], sep=separator)
+qc.sample.sid <- sapply(strsplit(qc.marker.samples, "_", fixed=TRUE), "[[", 3)
+qc.sample.pid <- sapply(qc.sample.sid, function(x) {
+	reg <- regexpr("[A-Z]+[0-9]+", x)
+	substring(x, reg[1], reg[1] + attr(reg, "match.length")[1] - 1)
+})
+qc.sample.mid <- sapply(strsplit(qc.marker.samples, "_", fixed=TRUE), "[[", 4)
+
+
+
+
 
 
 
@@ -329,6 +340,8 @@ dx.sample.pid <- sapply(dx.sample.sid, function(x) {
 })
 dx.sample.mid <- sapply(strsplit(dx.marker.samples, "_", fixed=TRUE), "[[", 4)
 dx.sample.ng <- as.numeric(gsub("ng", "", sapply(strsplit(dx.marker.samples, "_", fixed=TRUE), "[[", 5)))
+
+
 
 
 
@@ -598,12 +611,70 @@ dx.sample.clust <- lapply(1:length(dx.marker.samples), function(i) {
 						concentration=concentration, 
 						name=dx.marker.samples[i], 
 						n.outliers = n.outliers))
+
+	##QC
+
+
+	qc.samples <- qc.marker.samples[qc.sample.pid == pid & qc.sample.mid == mid]
+
+	qc.files <- qc.marker.files[qc.sample.pid == pid & qc.sample.mid == mid]
+
+
+	qc.results <- lapply(1:length(qc.samples), function(j) {
+		qc.file <- paste(folder, files[names(qc.samples)[j]], sep=separator)
+		qc.marker.int <- read.csv(qc.file, header=TRUE)
+		qc.marker.int <- qc.marker.int[sample(nrow(qc.marker.int)), ]
+
+
+		qc.mask <- (qc.marker.int[, gus.channel] < outlier.in.silence.channel$upper.bound) & (qc.marker.int[, gus.channel] > outlier.in.silence.channel$lower.bound) & (qc.marker.int[, marker.channel] < dx.marker.clust2$upper.bound)
+		
+		
+		if (!silence) {
+		
+			## get plot range
+			x.min <- min(c(qc.marker.int[, marker.channel], dx.marker.int[, marker.channel]))
+			x.max <- max(c(qc.marker.int[, marker.channel], dx.marker.int[, marker.channel]))
+			y.min <- min(c(qc.marker.int[, gus.channel], dx.marker.int[, gus.channel]))
+			y.max <- max(c(qc.marker.int[, gus.channel], dx.marker.int[, gus.channel]))
+			
+			
+			plot(qc.marker.int[,marker.channel], qc.marker.int[,gus.channel], 
+				    col = (qc.marker.int[,marker.channel] > dx.marker.clust2$threshold) + 1,
+					pch = c(4, 16)[qc.mask + 1],
+					main=paste(qc.samples[j], names(qc.samples)[j], 
+						"\n Click to remove, and click the middle key when done." ), 
+					xlab = "Channel 1", ylab="Channel 2", xlim=c(x.min, x.max), ylim=c(y.min, y.max))
+			lines(c(dx.marker.clust2$threshold, dx.marker.clust2$threshold), c(-1e8, 1e8), col=2, lwd=1.5)
+			qc.to.inverse <- identify(qc.marker.int[,1], qc.marker.int[,2], c("✓", "x")[qc.mask + 1], col="blue")
+			qc.mask[qc.to.inverse] <- !qc.mask[qc.to.inverse]
+		}
+		n.positive.droplets <- sum(qc.marker.int[,marker.channel][qc.mask] > dx.marker.clust2$threshold)
+		n.droplets <- sum(qc.mask)
+		n.outliers <- sum(!qc.mask)
+		concentration <- -log(1-(n.positive.droplets/n.droplets))/0.00085
+
+	
+	
+		list(intensities=qc.marker.int, 
+						threshold=dx.marker.clust2$threshold, 
+						mask=qc.mask, 
+						n.positive.droplets=n.positive.droplets, 
+						n.droplets = n.droplets, 
+						concentration=concentration, 
+						name=qc.samples[j],   
+						n.outliers = n.outliers)
+	})
+
+
+	qc.concentration <- qc.results[[1]]$concentration
+
+
+
+
 	## HL60 marker files
 	hl60.samples <- hl60.marker.samples[hl60.sample.pid == pid & hl60.sample.mid == mid]
 
-
 	hl60.files <- hl60.marker.files[hl60.sample.pid == pid & hl60.sample.mid == mid]
-
 
 
 	
@@ -710,6 +781,16 @@ dx.sample.clust <- lapply(1:length(dx.marker.samples), function(i) {
 	
 	fu.sids <- unique(fu.sample.sid[fu.sample.pid == pid & fu.sample.mid == mid])
 
+
+
+
+
+
+
+
+
+
+
 	
 	
 	for (fu.sid in fu.sids) {
@@ -721,13 +802,13 @@ dx.sample.clust <- lapply(1:length(dx.marker.samples), function(i) {
 
 		if (runType == "relative") {
 		
-				cat(paste("Processing ", fu.sid, "_", mid, ". Please key in the OFT/GUS ratio at Dx or hit enter to use the value calculated from the current test.\n", sep=""));
+				cat(paste("Processing ", fu.sid, "_", mid, ". Please key in the OFT/GUSB baseline ratio in Dx or hit enter to use the value calculated from the current test.\n", sep=""));
 				dx.baseline <- trimws(readLines("stdin",n=1))
 				dx.baseline <- as.numeric(dx.baseline)
 				if(is.na(dx.baseline)) {
-					cat("Using in-plate Dx baseline.\n")
+					cat("Using calculated Dx baseline ratio.\n")
 				} else {
-					cat("Using user specified Dx baseline.\n")
+					cat("Using preset Dx baseline ratio.\n")
 				
 				}
 		} else {
@@ -809,7 +890,7 @@ dx.sample.clust <- lapply(1:length(dx.marker.samples), function(i) {
 			dx.baseline=dx.baseline, 
 			hl60.gus.dilutionX=hl60.gus.dilutionX, 
 			hl60_baseline=hl60_baseline,
-			hl60.gus.concentration=hl60.gus.concentration, 
+			manual_hl60_baseline=manual_hl60_baseline,
 			run_by=run_by, 
 			run_date=run_date, 
 			reported_by=reported_by, 
@@ -817,6 +898,7 @@ dx.sample.clust <- lapply(1:length(dx.marker.samples), function(i) {
 			runType=runType,
 			is_manual_threshold=is_manual_threshold, 
 			manual_threshold=manual_threshold,
+			qc.concentration=qc.concentration,
 			date=Sys.time()), 
 			output_file = paste(folder, separator, fu.sid, "_", mid, "_", runmode, "_report.pdf", sep="")
 		)
@@ -835,7 +917,6 @@ dx.sample.clust <- lapply(1:length(dx.marker.samples), function(i) {
 			show_outliers=show_outliers, 
 			dx.baseline=dx.baseline, 
 			hl60.gus.dilutionX=hl60.gus.dilutionX, 
-			hl60.gus.concentration=hl60.gus.concentration, 
 			date=Sys.time()), 
 			output_file = paste(folder, separator, fu.sid, "_", mid, "_", runmode, "_2dFigs.pdf", sep="")
 		)
